94f281ca65bc9ed7d7d85a63b67cb21e42802125
Andreas Schulz <andi.schulz@me.com>
2019-04-22 23:12:02 +0200

    Add margin.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 from typing import List
 import sys
 
-from PyQt5.QtCore import QRectF
+from PyQt5.QtCore import QSize
 from PyQt5.QtGui import QColor, QResizeEvent
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QHBoxLayout, QVBoxLayout, QFrame, QGraphicsDropShadowEffect
 
@@ -16,15 +16,20 @@ class QSwitch(QFrame):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.enabled = True
-        self.setMinimumSize(32, 20)
         self.setUpLayers()
 
     def setUpLayers(self) -> None:
+        self.backgroundFrame = QFrame(self)
+        self.backgroundFrame.setObjectName('backgroundFrame')
+        self.backgroundFrame.setMinimumSize(32, 20)
+        self.setMinimumSize(36, 24)
+        self.backgroundFrame.move(2, 2)
+
         self.knob = QFrame(self)
         self.knob.setObjectName('knob')
         ef = QGraphicsDropShadowEffect()
         ef.setColor(QColor(0, 0, 0, 77))
-        ef.setBlurRadius(3)
+        ef.setBlurRadius(4)
         ef.setOffset(0, 2)
         self.knob.setGraphicsEffect(ef)
 
@@ -47,17 +52,22 @@ class QSwitch(QFrame):
         pass
 
     def reloadLayerSize(self) -> None:
-        self.setStyleSheet('QSwitch {{ border: 1px solid rgba(0, 0, 0, 51); border-radius: {0}px; }}'.format(int(self.height() / 2) - 1))
+        self.backgroundFrame.resize(self.width() - 4, self.height() - 4)
+        self.backgroundFrame.setStyleSheet('QFrame#backgroundFrame {{ border: 1px solid rgba(0, 0, 0, 51); border-radius: {0}px; }}'.format(int((self.height() - 4) / 2) - 1))
 
-        self.knob.resize(int((self.width() - 2) * (1 / kGoldenRatio)), self.height() - 2)
-        self.knob.move(1, 1)
+        self.knob.resize(int((self.width() - 4 - 2) * (1 / kGoldenRatio)), self.height() - 4 - 2)
+        self.knob.move(3, 3)
         self.knob.setStyleSheet('QFrame#knob {{ background-color: white; border-radius: {0}px; }}'.format(int(self.knob.height() / 2) - 1))
 
-        self.knobInside.resize(int((self.width() - 2) * (1 / kGoldenRatio)), self.height() - 2)
-        self.knobInside.move(1, 1)
+        self.knobInside.resize(int((self.width() - 4 - 2) * (1 / kGoldenRatio)), self.height() - 4 - 2)
+        self.knobInside.move(3, 3)
         self.knobInside.setStyleSheet('QFrame#knobInside {{ background-color: white; border-radius: {0}px; }}'.format(int(self.knobInside.height() / 2) - 1))
 
-        self.setMaximumHeight(self.knob.width() + 2)
+        self.setMaximumHeight(self.knob.width() + 2 + 4)
+
+    def sizeHint(self) -> QSize:
+        hint = self.backgroundFrame.sizeHint()
+        return QSize(hint.width() + 4, hint.height() + 4)
 
 
 def main(argv: List[str]) -> int:
